0b1d763a45531a498138826381056772eb586249
Kyle Domaszewicz <naezon@gmail.com>
2014-04-05 21:33:14 -0700

    Created method_invoke class.

---
+++ JLite/IR/Tree/method_invoke.java
@@ -0,0 +1,17 @@
+package IR.Tree;
+
+public class method_invoke extends expression
+{
+	String identifier;
+	Vector<argument> argument_list = new Vector<argument>(1,1); // contains all of the arguments
+	
+	public method_invoke(String i)
+	{
+		identifier = i;
+	}
+	
+	public addArgument(argument a)
+	{
+		argument_list.add(a);
+	}
+}
