fca2d582a7d20df39707627d2e80de66b4a756e0
Kyle Domaszewicz <naezon@gmail.com>
2014-04-07 10:50:28 -0700

    Fixed issue with naming conflict in Add class.

--- JLite/IR/Tree/add.java
+++ JLite/IR/Tree/add.java
@@ -2,11 +2,11 @@ package IR.Tree;
 
 import java.util.Vector;
 
-public class add extends expression
+public class Add extends expression
 {
 	Vector<String>Identifier = new Vector<String>(2,1);
 	
-	public add(String i)
+	public Add(String i)
 	{
 		Identifier.add(i);
 	}
