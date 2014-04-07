b57ef4f9a8764771a8db442b5f5ccc9c4b77ea30
Anthony Diep <diepa1@uci.edu>
2014-04-07 14:13:09 -0700

    Defined TypeDescriptor properties and methods.

--- JLite/IR/Tree/TypeDescriptor.java
+++ JLite/IR/Tree/TypeDescriptor.java
@@ -2,5 +2,16 @@ package IR.Tree;
 
 public class TypeDescriptor extends Descriptor
 {
-
+	private String Type;
+	
+	// Constructor
+	public TypeDescriptor(String t)
+	{
+		Type = t;
+	}
+	
+	public String getType()
+	{
+		return Type;
+	}
 }
