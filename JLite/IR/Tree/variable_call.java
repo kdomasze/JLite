ec1df041d28de69a19170267d1afdb66c4f1ff99
Kyle Domaszewicz <naezon@gmail.com>
2014-04-07 11:30:11 -0700

    Created variable_call class.

---
+++ JLite/IR/Tree/variable_call.java
@@ -0,0 +1,13 @@
+package IR.Tree;
+
+import java.util.Vector;
+
+public class variable_call extends expression
+{
+	String Identifier;
+	
+	public variable_call(String i)
+	{
+		Identifier = i;
+	}
+}
