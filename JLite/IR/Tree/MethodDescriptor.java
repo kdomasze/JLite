aef80533ce56282f1999376ed6fb2ebbda1ac72c
Kyle Domaszewicz <naezon@gmail.com>
2014-04-08 13:50:32 -0700

    Added MethodDescriptor's properties and methods.

--- JLite/IR/Tree/MethodDescriptor.java
+++ JLite/IR/Tree/MethodDescriptor.java
@@ -1,6 +1,32 @@
 package IR.Tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MethodDescriptor extends Descriptor
 {
-
+	private TreeNode ASTTree; // holds the AST Tree for the method
+	private Map<String, VarDescriptor> VarDescriptorMap = new HashMap<String, VarDescriptor>(); // hashMap for variable descriptors
+	
+	// put methods
+	public void createASTTree(TreeNode AST)
+	{
+		ASTTree = AST;
+	}
+	
+	public void addVarDescriptor(String Var, VarDescriptor VD)
+	{
+		VarDescriptorMap.put(Var, VD);
+	}
+	
+	// get methods
+	public TreeNode getASTTree()
+	{
+		return ASTTree;
+	}
+	
+	public VarDescriptor getVarDescriptor(String Var)
+	{
+		return VarDescriptorMap.get(Var);
+	}
 }
