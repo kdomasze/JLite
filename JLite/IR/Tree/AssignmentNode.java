6d6f751ca0bd805d3ec70e91dffc377b0d74e86f
Kyle Domaszewicz <naezon@gmail.com>
2014-04-08 15:54:58 -0700

    Created AssignmentNode's properties, constructor, and methods.

--- JLite/IR/Tree/AssignmentNode.java
+++ JLite/IR/Tree/AssignmentNode.java
@@ -2,5 +2,23 @@ package IR.Tree;
 
 public class AssignmentNode extends ExpressionNode
 {
-
+	private ExpressionNode leftHandSide;
+	private ExpressionNode rightHandSide;
+	
+	public AssignmentNode(ExpressionNode lhs, ExpressionNode rhs)
+	{
+		leftHandSide = lhs;
+		rightHandSide = rhs;
+	}
+	
+	// get methods
+	public ExpressionNode getLeftHandSide()
+	{
+		return leftHandSide;
+	}
+	
+	public ExpressionNode getRightHandSide()
+	{
+		return rightHandSide;
+	}
 }
