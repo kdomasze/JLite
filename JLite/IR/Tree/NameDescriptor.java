4cd3fae941e96b9f2b6af03f11803618ac29e236
Christian Becerra <cebecerr@uci.edu>
2014-04-07 18:37:20 -0700

    Defined Name Descriptor properties and methods

--- JLite/IR/Tree/NameDescriptor.java
+++ JLite/IR/Tree/NameDescriptor.java
@@ -2,5 +2,17 @@ package IR.Tree;
 
 public class NameDescriptor extends Descriptor
 {
-
+	private String name;
+	
+	// Constructor
+	public NameDescriptor(String n)
+	{
+		name = n;
+	}
+	
+	// get method
+	public String getName()
+	{
+		return name;
+	}
 }
