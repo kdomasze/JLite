1a6c8a9f3671ee56fc29041690fd9eccaf959007
Kyle Domaszewicz <naezon@gmail.com>
2014-04-08 15:17:41 -0700

    Added properties and methods for ReturnType and ParameterMap.

--- JLite/IR/Tree/MethodDescriptor.java
+++ JLite/IR/Tree/MethodDescriptor.java
@@ -5,28 +5,51 @@ import java.util.Map;
 
 public class MethodDescriptor extends Descriptor
 {
+	private TypeDescriptor ReturnType; // holds the return type of the Method
+	private Map<String, FieldDescriptor> ParameterMap = new HashMap<String, FieldDescriptor>(); // hashmap for the method parameters
 	private TreeNode ASTTree; // holds the AST Tree for the method
 	private Map<String, VarDescriptor> VarDescriptorMap = new HashMap<String, VarDescriptor>(); // hashMap for variable descriptors
 	
-	// put methods
+	// constructor
+	public MethodDescriptor(TypeDescriptor rt)
+	{
+		ReturnType = rt;
+	}
+	
+	// put methods	
+	public void addParameter(String Identifier, FieldDescriptor Parameter)
+	{
+		ParameterMap.put(Identifier, Parameter);
+	}
+	
 	public void createASTTree(TreeNode AST)
 	{
 		ASTTree = AST;
 	}
 	
-	public void addVarDescriptor(String Var, VarDescriptor VD)
+	public void addVarDescriptor(String Identifier, VarDescriptor VD)
 	{
-		VarDescriptorMap.put(Var, VD);
+		VarDescriptorMap.put(Identifier, VD);
 	}
 	
 	// get methods
+	public TypeDescriptor getReturnType()
+	{
+		return ReturnType;
+	}
+	
+	public FieldDescriptor getParameter(String Identifier)
+	{
+		return ParameterMap.get(Identifier);
+	}
+	
 	public TreeNode getASTTree()
 	{
 		return ASTTree;
 	}
 	
-	public VarDescriptor getVarDescriptor(String Var)
+	public VarDescriptor getVarDescriptor(String Identifier)
 	{
-		return VarDescriptorMap.get(Var);
+		return VarDescriptorMap.get(Identifier);
 	}
 }
