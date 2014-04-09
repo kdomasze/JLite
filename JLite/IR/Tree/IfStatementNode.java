fc649ef91cfd8d645c814ec215eeccec9a23b565
Christian Becerra <cebecerr@uci.edu>
2014-04-09 12:03:42 -0700

    Added body of Cast and IfStatement nodes

--- JLite/IR/Tree/IfStatementNode.java
+++ JLite/IR/Tree/IfStatementNode.java
@@ -2,5 +2,24 @@ package IR.Tree;
 
 public class IfStatementNode extends BlockStatementNode
 {
+	private OpNode condition;
+	private BlockNode block;
 	
+	//Constructor
+	IfStatementNode(OpNode c, BlockNode b)
+	{
+		condition = c;
+		block = b;
+	}
+	
+	//Get methods
+	public OpNode getOperation()
+	{
+		return condition;
+	}
+	
+	public BlockNode getBlock()
+	{
+		return block;
+	}
 }
