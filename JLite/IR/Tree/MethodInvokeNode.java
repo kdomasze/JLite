8d2200490b4a9c672effcfb91ed0d3c45dba0ece
Frank <fhpham@uci.edu>
2014-04-09 14:26:12 -0700

    made some changes - committed so i can pull

--- JLite/IR/Tree/MethodInvokeNode.java
+++ JLite/IR/Tree/MethodInvokeNode.java
@@ -2,5 +2,15 @@ package IR.Tree;
 
 public class MethodInvokeNode extends BlockStatementNode
 {
+	private NameDescriptor name;
+	private ExpressionNode args;
+	
+	//constructor
+	public MethodInvokeNode(NameDescriptor N, ExpressionNode A)
+	{
+		name = N;
+		args = A;
+
+	}
 	
 }
