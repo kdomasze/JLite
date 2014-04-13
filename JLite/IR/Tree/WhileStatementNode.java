f4e64e6f00eb45138f2d83afc9c9fbe357b17729
Anthony Diep <diepa1@uci.edu>
2014-04-12 20:12:13 -0700

    Added While Statement Node

---
+++ JLite/IR/Tree/WhileStatementNode.java
@@ -0,0 +1,25 @@
+package IR.Tree;
+
+public class WhileStatementNode extends BlockStatementNode
+{
+	private OpNode condition;
+	private BlockNode block;
+
+	//Constructor
+	WhileStatementNode(OpNode c, BlockNode b)
+	{
+		condition = c;
+		block = b;
+	}
+
+	//Get methods
+	public OpNode getOperation()
+	{
+		return condition;
+	}
+
+	public BlockNode getBlock()
+	{
+		return block;
+	}
+}
