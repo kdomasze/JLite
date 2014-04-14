be789c327e71d61e849faaca13dcdd27a678d5f3
Kyle Domaszewicz <kdomasze@uci.edu>
2014-04-13 21:51:34 -0700

    Symbol Tables for Field and Method Descriptor now have reference to parent in ClassDescriptor.

--- JLite/IR/ClassDescriptor.java
+++ JLite/IR/ClassDescriptor.java
@@ -5,14 +5,16 @@ import java.util.Map;
 
 public class ClassDescriptor extends Descriptor
 {
-	private SymbolTable FieldDescriptorSymbolTable = new SymbolTable(); // Symbol Table for field descriptors
-	private SymbolTable MethodDescriptorSymbolTable = new SymbolTable(); // Symbol Table for method descriptors
+	private SymbolTable FieldDescriptorSymbolTable; // Symbol Table for field descriptors
+	private SymbolTable MethodDescriptorSymbolTable; // Symbol Table for method descriptors
 	
 	private ClassDescriptor superClass; // holds reference for super/parent class
 	
 	// put methods
-	public void defineSuperClass(ClassDescriptor sC)
+	public void defineSuperClass(SymbolTable parent, ClassDescriptor sC)
 	{
+		FieldDescriptorSymbolTable = new SymbolTable(parent);
+		MethodDescriptorSymbolTable = new SymbolTable(parent);
 		superClass = sC;
 	}
 	
