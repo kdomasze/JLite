6c9acccda10dc3dff1985ae5c710e1b8a32e82ea
Kyle Domaszewicz <kdomasze@uci.edu>
2014-04-13 21:54:46 -0700

    Removed NameDescriptor reference from VarDescriptor.

--- JLite/IR/VarDescriptor.java
+++ JLite/IR/VarDescriptor.java
@@ -2,32 +2,24 @@ package IR;
 
 public class VarDescriptor extends Descriptor
 {
-
-	// MIGHT NEED TO REDO
-	private NameDescriptor name;
 	private TypeDescriptor type;
 	private ExpressionNode value;
 	
 	//constructor:
-	public VarDescriptor(NameDescriptor N, TypeDescriptor T, ExpressionNode V)
+	public VarDescriptor(TypeDescriptor T, ExpressionNode V)
 	{
-		name = N;
 		type = T;
 		value = V;
 	}
 	
 	//getMethods
-	public String getName()
+	
+	public TypeDescriptor getType()
 	{
-		return name.getName();
+		return type;
 	}
 	
-	public String getType()
-	{
-		return type.getType();
-	}
-	
-	public ExpressionNode getValue()
+	public ExpressionNode getValueNode()
 	{
 		return value;
 	}
