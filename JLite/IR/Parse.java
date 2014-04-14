ccc0466f69995d05813970c7667d4324524e034e
Christian Becerra <cebecerr@uci.edu>
2014-04-14 12:17:54 -0700

    Updated Parse.java

--- JLite/IR/Parse.java
+++ JLite/IR/Parse.java
@@ -1,6 +1,14 @@
 package IR;
 
+import Parse.ParseNode;
+
 public class Parse
 {
+	private ParseNode node;
+	private SymbolTable table;
 	
+	public void ParseClassDeclarer(SymbolTable table, ParseNode node)
+	{
+		
+	}
 }
