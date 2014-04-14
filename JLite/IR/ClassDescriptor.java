e9a61e9ff51f7d35c9e62426f66a776b689d4a0a
Kyle Domaszewicz <kdomasze@uci.edu>
2014-04-13 21:22:37 -0700

    Updated ClassDescriptor to use SymbolTables

--- JLite/IR/ClassDescriptor.java
+++ JLite/IR/ClassDescriptor.java
@@ -5,36 +5,26 @@ import java.util.Map;
 
 public class ClassDescriptor extends Descriptor
 {
-	private Map<NameDescriptor, FieldDescriptor> FieldDescriptorMap = new HashMap<NameDescriptor, FieldDescriptor>(); // hashMap for field descriptors
-	private Map<NameDescriptor, MethodDescriptor> MethodDescriptorMap = new HashMap<NameDescriptor, MethodDescriptor>(); // hashMap for method descriptors
+	private SymbolTable FieldDescriptorSymbolTable = new SymbolTable(); // Symbol Table for field descriptors
+	private SymbolTable MethodDescriptorSymbolTable = new SymbolTable(); // Symbol Table for method descriptors
 	
 	private ClassDescriptor superClass; // holds reference for super/parent class
 	
 	// put methods
-	public void addFieldDescriptor(NameDescriptor identifier, FieldDescriptor fD)
-	{
-		FieldDescriptorMap.put(identifier, fD);
-	}
-	
-	public void addMethodDescriptor(NameDescriptor identifier, MethodDescriptor mD)
-	{
-		MethodDescriptorMap.put(identifier, mD);
-	}
-	
 	public void defineSuperClass(ClassDescriptor sC)
 	{
 		superClass = sC;
 	}
 	
 	// get methods
-	public FieldDescriptor getFieldDescriptor(NameDescriptor identifier)
+	public SymbolTable getFieldDescriptorTable()
 	{
-		return FieldDescriptorMap.get(identifier);
+		return FieldDescriptorSymbolTable;
 	}
 	
-	public MethodDescriptor getMethodDescriptor(NameDescriptor identifier)
+	public SymbolTable getMethodDescriptorTable()
 	{
-		return MethodDescriptorMap.get(identifier);
+		return MethodDescriptorSymbolTable;
 	}
 	
 	public ClassDescriptor getSuperClass()
