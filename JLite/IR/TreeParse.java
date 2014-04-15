8aff687066178d16093c7707e0c8d8e8d9361219
Christian Becerra <cebecerr@uci.edu>
2014-04-15 13:37:49 -0700

    Added Parse thingy magingy

--- JLite/IR/TreeParse.java
+++ JLite/IR/TreeParse.java
@@ -31,7 +31,43 @@ public class TreeParse
 		
 	}
 	
-	public void ClassBody(SymbolTable table, ParseNode node)
+	public void ParseClassBody(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseField(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseMethod(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseMethodParameters(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseBlockBody(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseExpression(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseReturn(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseVarDeclaration(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseIfStatement(SymbolTable table, ParseNode node)
+	{
+		
+	}
+	public void ParseWhilteStatement(SymbolTable table, ParseNode node)
 	{
 		
 	}
