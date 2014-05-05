a243918aa9d91ed9f9b85b6b00d4dcdbdeee8f68
Kyle Domaszewicz <naezon@gmail.com>
2014-05-05 13:24:58 -0700

    Added Assignment Node

--- JLite/IR/Flat/TempDescriptor.java
+++ JLite/IR/Flat/TempDescriptor.java
@@ -1,15 +1,47 @@
 package IR.Flat;
+
 import IR.*;
 
-public class TempDescriptor extends Descriptor {
-  TypeDescriptor type;
+public class TempDescriptor extends Descriptor
+{
+	TypeDescriptor type;
+	int literal;
+	
+	private boolean litUndefined = true;
 
-  public TempDescriptor(String name, TypeDescriptor td) {
-    super(name);
-    type=td;
-  }
+	public TempDescriptor(String name, TypeDescriptor td)
+	{
+		super(name);
+		type = td;
+	}
 
-  public TypeDescriptor getType() {
-    return type;
-  }
+	public TempDescriptor(String name, TypeDescriptor td, int l)
+	{
+		super(name);
+		type = td;
+		literal = l;
+		litUndefined = false;
+	}
+
+	public TypeDescriptor getType()
+	{
+		return type;
+	}
+
+	public int getLiteral()
+	{
+		return literal;
+	}
+	
+	public String toString()
+	{
+		if(litUndefined == true)
+		{
+			return name + "(type: " + type + ")";
+		}
+		else
+		{
+			return name + "(type: " + type + ", value: " + literal + ")";
+		}
+	}
 }
