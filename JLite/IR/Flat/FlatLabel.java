936819f6e217ce78e27cfe4ceb030715ba255644
Christian Becerra <cebecerr@uci.edu>
2014-05-11 17:11:03 -0700

    Goto and Labels should be working now for IF

---
+++ JLite/IR/Flat/FlatLabel.java
@@ -0,0 +1,17 @@
+package IR.Flat;
+
+public class FlatLabel extends FlatNode 
+{
+	int numL;
+	
+	public FlatLabel(int num)
+	{
+		numL = num;
+	}
+
+	
+	public String toString()
+	{
+		return "L"+numL+":";
+	}
+}
